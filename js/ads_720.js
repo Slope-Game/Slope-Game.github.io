5f5a31204a6f6e7919514c532758a11d86dce355
unknown <hoaj@gcm.com>
2023-12-29 00:10:34 +0700

    update

---
+++ js/ads_720.js
@@ -0,0 +1,8 @@
+atOptions = {
+    'key' : '1057eb92bc5ed2a4f3d160639bcb15a3',
+    'format' : 'iframe',
+    'height' : 90,
+    'width' : 728,
+    'params' : {}
+};
+document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/1057eb92bc5ed2a4f3d160639bcb15a3/invoke.js"></scr' + 'ipt>');
